6998ecbe821c2eacbe25bf701837cd25bb7aa6d7
MarcinBaranek <baranekmarcin47@gmail.com>
2021-07-14 11:49:32 +0200

    add documentation to data_viewer

--- data_viewer.py
+++ data_viewer.py
@@ -31,4 +31,21 @@ def _show_as_film_z(image, start, end, step=1, sleep_time=1):
 
 
 def show_as_film(image, start=0, end=180, step=1, axis='x', sleep_time=1):
+    """ show slices of 3D image one by one.
+    !!! USE ONLY IN PYTHON CONSOLE !!!
+    ========
+    EXAMPLE:
+        import load_data
+        import data_viewer
+        img = load_data.load_data_as_array()
+        data_viewer.show_as_film(img, start=30, end=150, step=20)
+    ========
+    :param image: 3D image as numpy.array
+    :param start: int, first slice
+    :param end: int, last slice
+    :param step: int, difference between number of successive printed slices
+    :param axis: the axis along which the next slices will be displayed
+    :param sleep_time: time of displayed slices
+    :return: None
+    """
     globals()['_show_as_film_' + axis](image, start, end, step, sleep_time)
